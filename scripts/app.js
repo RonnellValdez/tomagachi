4a740baba3f1859a8b7c3676ff07a7ca51759c45
ronnellvjr <ronnell.b.valdez@gmail.com>
2021-04-06 14:23:59 -0700

    instatiated tomagachi and created hunger, sleepiness and boredom

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,25 @@
-console.log("hello");
+console.log("hello"); // js is connected to html
+const $name = "Jabba"; // will become user input later
+console.log($name);
+
+
+
+
+
+class Tomagachi {
+    constructor (nameProp){
+        this.name = nameProp;
+        
+        this.hunger = 10; //gives hunger a start of 10
+        this.sleep = 10;
+        this.play = 10; 
+    }
+
+//methods
+
+
+
+
+}
+const jabba = new Tomagachi($name); //creation of object jabba
+
